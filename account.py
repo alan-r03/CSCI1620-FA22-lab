bd29c0c05b756e786ea8d0580be0aa8dddf195a7
Alan Ramirez <alanramirez@unomaha.edu>
2022-11-18 17:50:00 -0600

    Account second iteration

--- account.py
+++ account.py
@@ -1,68 +1,37 @@
 class Account:
 
     def __init__(self, name):
-        self.account_name = name
-        self.account_balance = 0
+        self.account_name: string = name
+        self.account_balance: float = 0
 
     def is_float(self, amount):
-        """
-        Function to check if a string
-        can be converted to a float
-        :param amount: string value
-        :return: boolean True if
-        amount can be converted to
-        a float, else False
-        """
         try:
-            float(amount.strip())
+            float(amount)
+            print(True)
             return True
         except:
+            print(False)
             return False
 
     def deposit(self, amount):
-        """
-        Function to add amount
-        to an account's balance
-        :param amount: string value
-        :return: boolean True if
-        amount is added to account
-        balance, else False
-        """
-        if is_float(amount):
-            amount = float(amount.strip())
+        if self.is_float(f'{amount}'.strip()):
+            amount = float(f'{amount}'.strip())
+            print(amount, type(amount))
             if 0 < amount:
-                self.account_balance += amount
+                self.account_balance += round(amount, 2)
                 return True
         return False
 
     def withdraw(self, amount):
-        """
-        Function to subtract amount
-        from an account's balance
-        :param amount: string value
-        :return: boolean True if
-        amount is subtracted from
-        account balance, else False
-        """
-        if is_float(amount):
-            amount = float(amount.strip())
+        if self.is_float(f'{amount}'.strip()):
+            amount = float(f'{amount}'.strip())
             if 0 < amount <= self.account_balance:
-                self.account_balance -= amount
+                self.account_balance -= round(amount, 2)
                 return True
         return False
 
     def get_balance(self):
-        """
-        Function to retrieve account's
-        balance
-        :return: Float account balance
-        """
-        return self.account_balance
+        return round(self.account_balance, 2)
 
     def get_name(self):
-        """
-        Function to retrieve account's
-        name
-        :return: String account name
-        """
         return self.account_name
