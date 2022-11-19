b19f46a854e7d5902b3a11e4dce2fc5efc082229
Alan Ramirez <alanramirez@unomaha.edu>
2022-11-18 18:46:47 -0600

    Account docstring/type-hint iteration

--- account.py
+++ account.py
@@ -1,17 +1,44 @@
 class Account:
+    """
+    A class representing details for an Account object
+    """
 
-    def __init__(self, name):
-        self.account_name = name
-        self.account_balance = 0
+    def __init__(self, name: str, balance=0.0) -> None:
+        """
+        Constructor to create initial
+        state of an Account object
+        :param name: string value
+        :param balance: default
+        account balance
+        :return: None
+        """
+        self.account_name: string = str(name)
+        self.account_balance: float = balance
 
-    def is_float(self, amount):
+    def is_float(self, amount: float) -> bool:
+        """
+        Function to check if a string
+        can be converted to a float
+        :param amount: string value
+        :return: boolean True if
+        amount can be converted to
+        a float, else False
+        """
         try:
             float(amount)
             return True
         except:
             return False
 
-    def deposit(self, amount):
+    def deposit(self, amount: float) -> bool:
+        """
+        Function to add amount
+        to an account's balance
+        :param amount: string value
+        :return: boolean True if
+        amount is added to account
+        balance, else False
+        """
         if self.is_float(f'{amount}'.strip()):
             amount = float(f'{amount}'.strip())
             if 0 < amount:
@@ -19,7 +46,15 @@ class Account:
                 return True
         return False
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: float) -> bool:
+        """
+        Function to subtract amount
+        from an account's balance
+        :param amount: string value
+        :return: boolean True if
+        amount is subtracted from
+        account balance, else False
+        """
         if self.is_float(f'{amount}'.strip()):
             amount = float(f'{amount}'.strip())
             if 0 < amount <= self.account_balance:
@@ -27,8 +62,18 @@ class Account:
                 return True
         return False
 
-    def get_balance(self):
+    def get_balance(self) -> float:
+        """
+        Function to retrieve account's
+        balance
+        :return: Float account balance
+        """
         return round(self.account_balance, 2)
 
-    def get_name(self):
+    def get_name(self) -> str:
+        """
+        Function to retrieve account's
+        name
+        :return: String account name
+        """
         return self.account_name
