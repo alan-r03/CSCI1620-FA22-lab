fd7d9f440dccc6de5001f5995c50652708300ee3
alan-r03 <118496304+alan-r03@users.noreply.github.com>
2022-11-19 21:22:22 -0600

    Update account.py

--- account.py
+++ account.py
@@ -1,8 +1,8 @@
 class Account:
 
     def __init__(self, name):
-        self.account_name = str(name)
-        self.account_balance = 0
+        self.__account_name = str(name)
+        self.__account_balance = 0
 
     def is_float(self, amount):
         try:
@@ -15,20 +15,20 @@ class Account:
         if self.is_float(f'{amount}'.strip()):
             amount = float(f'{amount}'.strip())
             if 0 < amount:
-                self.account_balance += round(amount, 2)
+                self.__account_balance += round(amount, 2)
                 return True
         return False
 
     def withdraw(self, amount):
         if self.is_float(f'{amount}'.strip()):
             amount = float(f'{amount}'.strip())
-            if 0 < amount <= self.account_balance:
-                self.account_balance -= round(amount, 2)
+            if 0 < amount <= self.__account_balance:
+                self.__account_balance -= round(amount, 2)
                 return True
         return False
 
     def get_balance(self):
-        return round(self.account_balance, 2)
+        return round(self.__account_balance, 2)
 
     def get_name(self):
-        return self.account_name
+        return self.__account_name
